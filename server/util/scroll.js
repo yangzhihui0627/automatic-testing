fa7b91bf2c499bf69519a8a8c17ec9fe54c58c61
zhihui.yang <zhihui.yang@ptmind.com>
2019-01-10 14:43:25 +0800

    update add file

---
+++ server/util/scroll.js
@@ -0,0 +1,28 @@
+function wait (ms) {
+  return new Promise(resolve => setTimeout(() => resolve(), ms));
+  }
+
+  await page.goto('http://www.yangzhihuiweb.com', {waitUntil: 'load'});
+  // Get the height of the rendered page
+  const bodyHandle = await page.$('body');
+  const { height } = await bodyHandle.boundingBox();
+  await bodyHandle.dispose();
+
+  // Scroll one viewport at a time, pausing to let content load
+  const viewportHeight = page.viewport().height;
+  let viewportIncr = 0;
+  while (viewportIncr + viewportHeight < height) {
+    await page.evaluate(_viewportHeight => {
+      window.scrollBy(0, _viewportHeight);
+    }, viewportHeight);
+    await wait(2000);
+    viewportIncr = viewportIncr + viewportHeight;
+  }
+
+  // Scroll back to top
+  await page.evaluate(_ => {
+    window.scrollTo(0, 0);
+  });
+
+  // Some extra delay to let images load
+  await wait(2000);
