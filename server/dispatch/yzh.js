0e2d32abe3501801a6a957ea9084b9993bcbbded
zhihui.yang <zhihui.yang@ptmind.com>
2019-02-15 18:07:20 +0800

    add dispatch folder

---
+++ server/dispatch/yzh.js
@@ -0,0 +1,14 @@
+/**
+ * 调度执行哪些模版，控制执行流程和次数
+ */
+const puppeteer = require('puppeteer');
+const os = require('../util/scroll.js');
+const device = require('../util/device.js');
+const monitor = require('../util/monitor.js');
+
+const start = async (index) =>{
+	//获取要执行的模版
+	let templates = 
+}
+
+start();
